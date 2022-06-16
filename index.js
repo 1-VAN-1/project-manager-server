087ab8dd478684dccedea00f350d0fc5e7d333af
Ivan Zakharyan <zakharyan-0@mail.ru>
2022-06-16 23:54:11 +0300

    Fix

--- index.js
+++ index.js
@@ -28,6 +28,12 @@ routers.forEach((router) => {
 
 app.use(express.static(path.resolve(__dirname, "static")));
 
+app.get("/static/uploads/:filename", (req, res) => {
+  res.sendFile(
+    path.resolve(__dirname, "static", "uploads", req.params.filename)
+  );
+});
+
 app.use(
   history({
     index: path.resolve(__dirname, "static", "index.html"),
@@ -36,12 +42,6 @@ app.use(
 
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "static", "index.html"));
-});
-
-app.get("/static/uploads/:filename", (req, res) => {
-  res.sendFile(
-    path.resolve(__dirname, "static", "uploads", req.params.filename)
-  );
 });
 
 app.use(errorMiddleware);
